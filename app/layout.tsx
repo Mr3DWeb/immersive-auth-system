ff369d2478066738756a8169a777157fbe83f113
Mohammad Sharyatzadeh <mr3dweb@gmail.com>
2026-02-17 16:12:30 +0330

    Fix fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,42 @@
 import type { Metadata } from "next";
+import localFont from 'next/font/local'
 import "./globals.css";
 
+const parkinsans = localFont({
+  src: [
+    {
+      path: './fonts/Parkinsans-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Parkinsans-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Parkinsans-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Parkinsans-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Parkinsans-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-parkinsans',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Immersive Auth System",
-  description: "Immersive Auth System",
+  description: "Experience the next generation of authentication",
 };
 
 export default function RootLayout({
@@ -14,7 +46,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={parkinsans.className}>
         {children}
       </body>
     </html>
